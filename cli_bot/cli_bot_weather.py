30809a319c9da7c88f4c17f5a1a860206745fec9
Max <testspb777@gmail.com>
2026-03-10 21:13:56 +0300

    update cli_bot_weather.py

--- cli_bot/cli_bot_weather.py
+++ cli_bot/cli_bot_weather.py
@@ -1,7 +1,6 @@
 import os
 
 import requests
-import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,16 +9,15 @@ user_input = input("Введите город, что бы узнать темп
 
 API_KEY = os.getenv("API_KEY")
 
-
 url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={user_input}'
 
 response = requests.post(url)
 
 if response.status_code == 200:
     date = response.json()
-    dat = json.dumps(date)
-    print(f"Bot: температура по цельсию: {date['location']['country']}")
-    print(f"---: температура по цельсию: {date['current']['temp_c']}")
-    print(f"---: температура по фаренгейту: {date['current']['temp_f']}")
+    print(f"Город: {date['location']['name']}")
+    print(f"Cтрана: {date['location']['country']}")
+    print(f"Температура по Цельсию: {date['current']['temp_c']}")
+    print(f"Температура по Фаренгейту: {date['current']['temp_f']}")
 else:
     print(f'Error:{response.status_code}')
